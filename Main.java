a93a4b57d90ecf68de8c212ea776e747be3004c7
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2024-05-17 17:49:10 +0200

    Main class template with Doxygen-style comments

---
+++ Main.java
@@ -0,0 +1,22 @@
+/**
+ * @file Main.java
+ * @brief This file contains the Main class.
+ */
+package scheduling_algorithms;
+
+/**
+ * @class Main
+ * @brief Contains the main function that kick-starts
+ * 		  the Scheduling-Algorithms application.
+ */
+public class Main {
+
+	/**
+	 * @method main
+	 * @brief The main method to start the Scheduling-Algorithms application.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Scheduling-Algorithms");
+	}
+
+}

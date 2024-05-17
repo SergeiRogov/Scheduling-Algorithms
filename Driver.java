322cd5c32c4106a32cc318e9936373dd2c0ca474
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2024-05-17 19:02:05 +0200

    Rename Main -> Driver

---
+++ Driver.java
@@ -0,0 +1,22 @@
+/**
+ * @file Driver.java
+ * @brief This file contains the Driver class.
+ */
+package scheduling_algorithms;
+
+/**
+ * @class Driver
+ * @brief Contains the main function that kick-starts
+ * 		  the Scheduling-Algorithms application.
+ */
+public class Driver {
+
+	/**
+	 * @method main
+	 * @brief The main method to start the Scheduling-Algorithms application.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Scheduling-Algorithms Driver");
+	}
+
+}

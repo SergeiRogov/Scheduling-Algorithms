20192f9e0ec922d30cd7c236701e4ee703003878
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2024-05-17 18:39:28 +0200

    Algorithm interface + Task class

---
+++ Algorithm.java
@@ -0,0 +1,32 @@
+/**
+ * @file Algorithm.java
+ * @brief This file contains a generic scheduling Algorithm interface.
+ */
+package scheduling_algorithms;
+
+/**
+ * @interface Algorithm
+ * @brief Generic scheduling algorithm interface. 
+ * 
+ *        Contains 2 methods:
+ *        	1) 	schedule() method for obtaining 
+ *        		the next task to be run on the CPU.
+ *        
+ *        	2) 	pickNetTask() method pick next task 
+ *        		to be executed.
+ */
+public interface Algorithm {
+	
+	/**
+	 * @method schedule
+	 * @brief Implementation of a scheduling algorithm.
+	 */
+	public void schedule();
+	
+	/**
+	 * @method pickNetTask
+	 * @brief Selects the next task to be scheduled.
+	 */
+	public Task pickNetTask();
+	
+}
---
+++ Task.java
@@ -0,0 +1,85 @@
+/**
+ * @file Task.java
+ * @brief This file contains a Task Class.
+ */
+package scheduling_algorithms;
+
+/**
+ * @class Task
+ * @brief A task to be scheduled.
+ * 		  Has the form [task name] [priority] [CPU burst]
+ */
+public class Task {
+	private String taskName;
+    private int priority;
+    private int cpuBurst;
+    
+    /**
+     * @constructor Task To initialize a Task with the given name, priority, and CPU burst.
+     * @param taskName The name of the task.
+     * @param priority The priority of the task.
+     * @param cpuBurst The CPU burst time of the task.
+     */
+    public Task(String taskName, int priority, int cpuBurst) {
+    	setTaskName(taskName);
+    	setPriority(priority);
+    	setCpuBurst(cpuBurst);
+    }
+    
+    /**
+     * @method getTaskName Gets the name of the task.
+     * @return The name of the task.
+     */
+    public String getTaskName() {
+        return taskName;
+    }
+    
+    /**
+     * @method setTaskName Sets the name of the task.
+     * @param taskName The name of the task.
+     */
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    /**
+     * @method getPriority Gets the priority of the task.
+     * @return The priority of the task.
+     */
+    public int getPriority() {
+        return priority;
+    }
+
+    /**
+     * @method setPriority Sets the priority of the task.
+     * @param priority The priority of the task.
+     */
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    /**
+     * @method getCpuBurst Gets the CPU burst time of the task.
+     * @return The CPU burst time of the task.
+     */
+    public int getCpuBurst() {
+        return cpuBurst;
+    }
+
+    /**
+     * @method setCpuBurst Sets the CPU burst time of the task.
+     * @param cpuBurst The CPU burst time of the task.
+     */
+    public void setCpuBurst(int cpuBurst) {
+        this.cpuBurst = cpuBurst;
+    }
+
+    /**
+     * @method toString Representation a task as a string.
+     * @return String representation of a task.
+     */
+    @Override
+    public String toString() {
+        return "Task [taskName=" + this.taskName + ", priority=" + this.priority + ", cpuBurst=" + this.cpuBurst + "]";
+    }
+}

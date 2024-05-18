8d4baeb5f313a4065bd9828251d313198b08ae8c
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2024-05-19 00:20:36 +0200

    Added TaskReader - reads from schedule.txt
    Inserts each task into a Java ArrayList

--- Driver.java
+++ Driver.java
@@ -4,19 +4,24 @@
  */
 package scheduling_algorithms;
 
+import java.util.ArrayList;
+
 /**
  * @class Driver
  * @brief Contains the main function that kick-starts
  * 		  the Scheduling-Algorithms application.
  */
 public class Driver {
-
+	
 	/**
 	 * @method main
 	 * @brief The main method to start the Scheduling-Algorithms application.
 	 */
 	public static void main(String[] args) {
+		ArrayList<Task> tasks = TaskReader.readTasks();
+		
 		System.out.println("Scheduling-Algorithms Driver");
+		System.out.println(tasks);
 	}
 
 }
---
+++ TaskReader.java
@@ -0,0 +1,46 @@
+/**
+ * @file TaskReader.java
+ * @brief This file contains the TaskReader class.
+ */
+package scheduling_algorithms;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * @class TaskReader
+ * @brief Contains utility methods to read tasks from a file.
+ */
+public class TaskReader {
+
+	/**
+     * @method readTasks
+     * @brief Reads in the schedule of tasks from a schedule.txt file 
+     *        and inserts each task into a Java ArrayList.
+     * @return ArrayList of tasks.
+     */
+    public static ArrayList<Task> readTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        String fileName = "schedule.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(", ");
+                if (parts.length == 3) {
+                    String taskName = parts[0];
+                    int priority = Integer.parseInt(parts[1]);
+                    int cpuBurst = Integer.parseInt(parts[2]);
+                    Task task = new Task(taskName, priority, cpuBurst);
+                    tasks.add(task);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return tasks;
+    }
+}

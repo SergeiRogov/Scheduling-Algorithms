29433085c362fbaf84c944d200da8c9bf7116912
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2024-05-19 17:02:49 +0200

    RR implementation

--- RR.java
+++ RR.java
@@ -4,20 +4,37 @@
  */
 package scheduling_algorithms;
 
+import java.util.ArrayList;
+
 /**
  * @class RR
  * @brief Implementation of Round-Robin scheduling algorithm.
  */
 public class RR implements Algorithm {
+	
+	private static final int TIME_QUANTUM = 20;
 
 	/**
 	 * @method schedule
 	 * @brief Implementation of a RR scheduling algorithm.
 	 */
 	@Override
-	public void schedule() {
-		// TODO Auto-generated method stub
-		
+	public void schedule(ArrayList<Task> tasks) {
+		System.out.println("\nScheduling with RR...");
+		while (!tasks.isEmpty()) {
+            Task task = pickNextTask(tasks);
+            if (task != null) {
+            	if (task.getCpuBurst() <= TIME_QUANTUM) {
+            		CPU.run(task);
+            	} else {
+            		CPU.run(new Task(task.getTaskName(), task.getPriority(), TIME_QUANTUM));
+            		task.setCpuBurst(task.getCpuBurst() - TIME_QUANTUM);
+            		tasks.add(task);
+            	}
+                
+            }
+        }
+		System.out.println("Completed!");
 	}
 
 	/**
@@ -25,9 +42,22 @@ public class RR implements Algorithm {
 	 * @brief Selects the next task to be scheduled with RR algorithm.
 	 */
 	@Override
-	public Task pickNextTask() {
-		// TODO Auto-generated method stub
-		return null;
+	public Task pickNextTask(ArrayList<Task> tasks) {
+		
+		if (tasks.isEmpty()) {
+			return null; // No task available
+		}
+		
+		return tasks.remove(0);
 	}
+	
+	/**
+     * @method toString Representation an algorithm as a string.
+     * @return String representation of an algorithm.
+     */
+    @Override
+    public String toString() {
+        return "RR algorithm";
+    }
 
 }

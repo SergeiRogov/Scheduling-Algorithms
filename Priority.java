29af779d95490b8d2835a7b90eb32ceaa63c4726
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2024-05-19 22:42:22 +0300

    Update Priority.java
    Turnaround, Waiting and Response Time calculation added

--- Priority.java
+++ Priority.java
@@ -19,10 +19,29 @@ public class Priority implements Algorithm {
 	@Override
 	public void schedule(ArrayList<Task> tasks) {
 		System.out.println("\nScheduling with Priority...");
+		
+		int currentTime = 0; // Current time in the CPU schedule
+		
 		while (!tasks.isEmpty()) {
             Task task = pickNextTask(tasks);
             if (task != null) {
+            	task.setWaitingTime(currentTime - task.getArrivalTime());
+            	
+            	if (task.getResponseTime() == 0) {
+                    task.setResponseTime(currentTime - task.getArrivalTime());
+                }
+            	
+            	
                 CPU.run(task);
+                currentTime += task.getCpuBurst();
+                
+                
+                task.setTurnaroundTime(currentTime-task.getArrivalTime());
+                
+                
+                System.out.println("Task " + task.getTaskName() + ": Waiting Time = " + task.getWaitingTime() +
+                        ", Turnaround Time = " + task.getTurnaroundTime() +
+                        ", Response Time = " + task.getResponseTime());
             }
         }
 		System.out.println("Completed!");

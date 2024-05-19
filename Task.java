6f24ea08f5dc205c7aadbdbb7da937a31abf4873
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2024-05-19 20:31:57 +0300

    Update Task.java

--- Task.java
+++ Task.java
@@ -14,6 +14,10 @@ public class Task {
 	private String taskName;
     private int priority;
     private int cpuBurst;
+    private int arrivalTime; 
+    private int waitingTime; 
+    private int responseTime; 
+    private int turnaroundTime; 
     
     /**
      * @constructor Task To initialize a Task with the given name, priority, and CPU burst.
@@ -25,6 +29,10 @@ public class Task {
     	setTaskName(taskName);
     	setPriority(priority);
     	setCpuBurst(cpuBurst);
+	this.waitingTime = 0;
+	this.arrivalTime = 0;
+	this.responseTime = 0;
+        this.turnaroundTime = 0;
     }
     
     /**
@@ -74,6 +82,71 @@ public class Task {
     public void setCpuBurst(int cpuBurst) {
         this.cpuBurst = cpuBurst;
     }
+
+    /**
+     * @method getArrivalTime Gets the arrival time of the task.
+     * @return The arrival time of the task.
+     */
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    /**
+     * @method setArrivalTime Sets the arrival time of the task.
+     * @param arrivalTime The arrival time of the task.
+     */
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    /**
+     * @method getWaitingTime Gets the waiting time of the task.
+     * @return The waiting time of the task.
+     */
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    /**
+     * @method setWaitingTime Sets the waiting time of the task.
+     * @param waitingTime The waiting time of the task.
+     */
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    /**
+     * @method getTurnaroundTime Gets the turnaround time of the task.
+     * @return The turnaround time of the task.
+     */
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    /**
+     * @method setTurnaroundTime Sets the turnaround time of the task.
+     * @param turnaroundTime The turnaround time of the task.
+     */
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    /**
+     * @method getArrivalTime Gets the arrival time of the task.
+     * @return The arrival time of the task.
+     */
+    public int getResponseTime() {
+        return responseTime;
+    }
+
+    /**
+     * @method setArrivalTime Sets the arrival time of the task.
+     * @param arrivalTime The arrival time of the task.
+     */
+    public void setResponseTime(int responseTime) {
+        this.responseTime = responseTime;
+    }
+
 
     /**
      * @method toString Representation a task as a string.
